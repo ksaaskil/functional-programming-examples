1f80cfd6ed00c385f89fbb42b51b28d09e8014d6
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-08 16:39:26 +0300

    Work on the task either example.

--- fp-ts/taskeither.test.ts
+++ fp-ts/taskeither.test.ts
@@ -5,10 +5,18 @@ import {
   map,
   fromEither,
 } from "fp-ts/lib/TaskEither";
-import { either, tryCatch as eitherTryCatch } from "fp-ts/lib/either";
+import {
+  either,
+  Either,
+  fold,
+  isLeft,
+  isRight,
+  getOrElse,
+  tryCatch as eitherTryCatch,
+} from "fp-ts/lib/either";
 import { pipe } from "fp-ts/lib/pipeable";
 import { Lazy } from "fp-ts/lib/function";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import chalk from "chalk";
 
 const ICNDB_HOST = "http://api.icndb.com";
@@ -16,52 +24,81 @@ const ICNDB_HOST = "http://api.icndb.com";
 const colorJoke = chalk.bold.magentaBright;
 const url = `${ICNDB_HOST}/jokes/random`;
 
-const chuckClient = async (): Promise<string> => {
-  const response = await axios.get(url);
-  const joke = response.data.value.joke;
-  return joke;
-};
-
+/**
+ * Fetch response body from URL.
+ * @param url Target URL
+ */
 const fetch = async (url: string) => {
-  return await axios(url);
+  return (await axios(url)).data;
 };
-
-type ResponseBody = any;
-
-const fpFetch = (url: string): TaskEither<Error, ResponseBody> =>
-  tryCatch(
-    async () => {
-      const axiosResponse = await fetch(url);
-      return axiosResponse.data;
-    },
-    (e: unknown) => new Error(`Failed fetching from ${url}`)
-  );
-
-const liftToEither = <A>(f: Lazy<A>, failureMessage?: string) =>
-  eitherTryCatch(f, (e: unknown) =>
-    Error(failureMessage || `Operation failed: ${JSON.stringify(e)}`)
-  );
 
 const parseResponseBody = (responseBody: any): string =>
   responseBody.value.joke;
 
+const chuckClient = async (): Promise<string> => {
+  const response = await fetch(url);
+  const joke = parseResponseBody(response);
+  return joke;
+};
+
+type ResponseBody = any;
+
+const fetchJokeTask = (url: string): TaskEither<Error, ResponseBody> =>
+  tryCatch(
+    () => fetch(url),
+    (e: unknown) => new Error(`Failed fetching from ${url}`)
+  );
+
+const asTaskEither = <A>(
+  f: Lazy<A>,
+  failureMessage?: string
+): TaskEither<Error, A> => fromEither(asEither(f, failureMessage));
+
+const asEither = <A>(f: Lazy<A>, failureMessage?: string) =>
+  eitherTryCatch(f, (e: unknown) =>
+    Error(failureMessage || `Operation failed: ${JSON.stringify(e)}`)
+  );
+
+const parseResponseBodyAsTaskEither = (
+  responseBody: any
+): TaskEither<Error, string> =>
+  asTaskEither(
+    () => parseResponseBody(responseBody),
+    `Failed parsing joke from response: ${JSON.stringify(responseBody)}`
+  );
+
+// Problem: chaining a `TaskEither<Error, string>` with `string => Either<Error, string>`
+// (without casting everything with `asTaskEither`) is possible how?
 const fpChuckClient = (url: string): TaskEither<Error, string> =>
   pipe(
     url,
-    fpFetch,
-    chain(responseBody =>
-      fromEither(
-        liftToEither(
-          () => parseResponseBody(responseBody),
-          `Failed parsing joke from response`
-        )
-      )
-    )
+    fetchJokeTask,
+    chain(parseResponseBodyAsTaskEither)
   );
 
+/**
+ * Get the right value or throw.
+ * @param ma Instance of either
+ */
+const get = <A, B>(ma: Either<A, B>): B => {
+  return fold(
+    e => {
+      throw e;
+    },
+    (val: B) => val
+  )(ma);
+};
+
 describe("Getting a random joke", () => {
-  it("works", async () => {
+  it("works imperatively", async () => {
     const joke = await chuckClient();
+    console.log(`Your daily Chuck joke: ${colorJoke(joke)}`);
+    expect(joke.length).toBeGreaterThan(0);
+  });
+  it("works with FP", async () => {
+    const jokeTask: TaskEither<Error, string> = fpChuckClient(url);
+    const resolvedJoke: Either<Error, string> = await jokeTask();
+    const joke = get(resolvedJoke);
     console.log(`Your daily Chuck joke: ${colorJoke(joke)}`);
     expect(joke.length).toBeGreaterThan(0);
   });
