94ee6fa0bedcf21fc2c9d8b4c8d60abcd0ab2cf2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-30 16:36:57 +0300

    Add solution to Problem 1 with HUnit tests (quite awful tests).

--- project-euler/src/Problem1.hs
+++ project-euler/src/Problem1.hs
@@ -1,8 +1,10 @@
-module Problem1
-    ( result
-    )
-where
+module Problem1 where
+
+resultForBelow :: (Int -> Int)
+resultForBelow maxValue =
+    let array    = takeWhile (< maxValue) [0 ..]
+        filtered = filter (\n -> n `mod` 3 == 0 && n `mod` 5 == 0) array
+    in  sum filtered
 
 result :: Int
-result = let a = 1 in a + 1
-
+result = resultForBelow 1000
--- project-euler/test/Spec.hs
+++ project-euler/test/Spec.hs
@@ -1,10 +1,17 @@
 import           Test.HUnit
-import           Problem1                       ( result )
+import           Problem1                       ( result
+                                                , resultForBelow
+                                                )
 
-testProblem1 :: Test
-testProblem1 = TestCase $ assertEqual "Should return 2" 2 result
+testProblem1ResultForBelow = TestCase
+    $ assertEqual "Should return 15 for under 20" 15 (resultForBelow 20)
 
-tests = TestList [TestLabel "Problem 1" testProblem1]
+testProblem1Result = TestCase $ assertEqual "Should return 33165" 33165 result
+
+tests = TestList
+    [ TestLabel "Problem 1"        testProblem1Result
+    , TestLabel "Problem 1 helper" testProblem1ResultForBelow
+    ]
 
 main :: IO Counts
 main = runTestTT tests
