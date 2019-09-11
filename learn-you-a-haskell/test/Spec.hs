6c74db10721c0a655385d7d60bd5f6e3520b6212
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-11 18:43:27 +0300

    Add example test.

--- learn-you-a-haskell/test/Spec.hs
+++ learn-you-a-haskell/test/Spec.hs
@@ -1,4 +1,8 @@
-import           Test.HUnit
+import           Test.HUnit                    as HUnit
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+test1 = TestCase $ assertEqual "Should 1+1 equal 2" 2 (1 + 1)
+
+tests = TestList [TestLabel "Example test 1" test1]
+
+main :: IO HUnit.Counts
+main = HUnit.runTestTT tests
