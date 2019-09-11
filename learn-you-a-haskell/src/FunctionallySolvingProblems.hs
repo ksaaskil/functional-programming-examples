1387189aa744a4f0c43d86e0a4d46ab2f12db21d
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-11 19:31:49 +0300

    Add more point-free stuff.

--- learn-you-a-haskell/src/FunctionallySolvingProblems.hs
+++ learn-you-a-haskell/src/FunctionallySolvingProblems.hs
@@ -12,10 +12,7 @@ import           Data.List                     as L
 
 -- The type for the calculator would be, a function from string to number
 solveRPN :: (Num a, Read a) => String -> a
-solveRPN expression =
-    let splitted = words expression
-        folded   = foldl foldingFunction [] splitted
-    in  head folded
+solveRPN = head . foldl foldingFunction [] . words
   where
     foldingFunction (x : y : ys) "*"          = (x * y) : ys
     foldingFunction (x : y : ys) "+"          = (x + y) : ys
