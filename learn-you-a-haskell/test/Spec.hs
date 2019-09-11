a29ff463d74000dd6ed1e19c9a9c8fffb60f5d14
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-11 18:47:05 +0300

    Add simple test for RPN.

--- learn-you-a-haskell/test/Spec.hs
+++ learn-you-a-haskell/test/Spec.hs
@@ -1,8 +1,13 @@
+import           FunctionallySolvingProblems    ( solveRPN )
 import           Test.HUnit                    as HUnit
 
 test1 = TestCase $ assertEqual "Should 1+1 equal 2" 2 (1 + 1)
+testRPN1 =
+    TestCase $ assertEqual "Should handle summation" 2 $ solveRPN "1 1 +"
 
-tests = TestList [TestLabel "Example test 1" test1]
+
+tests = TestList
+    [TestLabel "Example test 1" test1, TestLabel "Solve RPN 1" testRPN1]
 
 main :: IO HUnit.Counts
 main = HUnit.runTestTT tests
