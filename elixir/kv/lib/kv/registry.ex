5eb30404889f69bbae860bed259972f647076210
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-21 10:23:52 +0200

    Format.

--- elixir/kv/lib/kv/registry.ex
+++ elixir/kv/lib/kv/registry.ex
@@ -1,43 +1,43 @@
 defmodule KV.Registry do
-    use GenServer
+  use GenServer
 
-    # Client API will be here
+  # Client API will be here
 
-    def start_link(opts) do
-        # Callbacks defined in current __MODULE__
-        GenServer.start_link(__MODULE__, :ok, opts)
+  def start_link(opts) do
+    # Callbacks defined in current __MODULE__
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  # Callbacks
+
+  @impl true
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  # The "@impl true" informs the compiler that our intention for the subsequent function definition is to define a callback.
+  # Calls are synchronous and the server must send back a response
+  @impl true
+  def handle_call({:lookup, name}, _from, names) do
+    {:reply, Map.fetch(names, name), names}
+  end
+
+  # Casts are asynchronous, the server won't send a response back
+  @impl true
+  def handle_cast({:create, name}, names) do
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.start_link([])
+      {:noreply, Map.put(names, name, bucket)}
     end
-
-    def lookup(server, name) do
-        GenServer.call(server, {:lookup, name})
-    end
-
-    def create(server, name) do
-        GenServer.cast(server, {:create, name})
-    end
-
-    # Callbacks
-
-    @impl true
-    def init(:ok) do
-        {:ok, %{}}
-    end
-
-    # The "@impl true" informs the compiler that our intention for the subsequent function definition is to define a callback.
-    # Calls are synchronous and the server must send back a response
-    @impl true
-    def handle_call({:lookup, name}, _from, names) do
-        {:reply, Map.fetch(names, name), names}
-    end
-
-    # Casts are asynchronous, the server won't send a response back
-    @impl true
-    def handle_cast({:create, name}, names) do
-        if Map.has_key?(names, name) do
-            {:noreply, names}
-        else
-            {:ok, bucket} = KV.Bucket.start_link([])
-            {:noreply, Map.put(names, name, bucket)}
-        end
-    end
+  end
 end
--- elixir/kv/test/kv/registry_test.exs
+++ elixir/kv/test/kv/registry_test.exs
@@ -1,23 +1,26 @@
 defmodule KV.RegistryTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    setup do
-        # The start_supervised! function is injected by use ExUnit.Case. It does the job of starting the KV.Registry process, by calling its start_link/1 function. The advantage of using start_supervised! is that ExUnit will guarantee that the registry process will be shutdown before the next test starts.
-        registry = start_supervised!(KV.Registry)
-        %{registry: registry}
-    end
+  setup do
+    # The start_supervised! function is injected by use ExUnit.Case. It does the job of starting the KV.Registry process, by calling its start_link/1 function. The advantage of using start_supervised! is that ExUnit will guarantee that the registry process will be shutdown before the next test starts.
+    registry = start_supervised!(KV.Registry)
+    %{registry: registry}
+  end
 
-    test "spawns buckets", %{registry: registry} do
-        assert KV.Registry.lookup(registry, "shopping") == :error
+  test "spawns buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
 
-        KV.Registry.create(registry, "shopping")
-        assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
-        KV.Bucket.put(bucket, "milk", 1)
-        assert KV.Bucket.get(bucket, "milk") == 1
-    end
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
 
-    test "removes buckets on exit", %{registry: registry} do
-        KV.Registry.create(registry, "shopping")
-    end
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
