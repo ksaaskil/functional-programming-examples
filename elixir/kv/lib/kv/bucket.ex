b5492f305ac5c68175c2b659b1437006fa8469e9
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-21 09:38:26 +0200

    Add stuff.

--- elixir/kv/lib/kv/bucket.ex
+++ elixir/kv/lib/kv/bucket.ex
@@ -1,24 +1,28 @@
 defmodule KV.Bucket do
-    use Agent
+  use Agent
 
-    @doc """
-    Starts a new bucket agent.
-    """
-    def start_link(_opts) do
-        Agent.start_link(fn -> %{} end)
-    end
+  @doc """
+  Starts a new bucket agent.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
 
-    @doc """
-    Gets a value from bucket agent with key.
-    """
-    def get(bucket, key) do
-        Agent.get(bucket, &Map.get(&1, key))
-    end
+  @doc """
+  Gets a value from bucket agent with key.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
 
-    @doc "
+  @doc "
     Put a value to bucket with key and value.
     "
-    def put(bucket, key, value) do
-        Agent.update(bucket, &Map.put(&1, key, value))
-    end
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
--- elixir/kv/test/kv/bucket_test.exs
+++ elixir/kv/test/kv/bucket_test.exs
@@ -1,11 +1,39 @@
 defmodule KV.BucketTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    test "stores values by key" do
-        {:ok, bucket } = KV.Bucket.start_link([])
-        assert KV.Bucket.get(bucket, "milk") == nil
+  # setup macro run before every test in the same process as the test
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
 
-        KV.Bucket.put(bucket, "milk", 3)
-        assert KV.Bucket.get(bucket, "milk") == 3
-    end
+  # Pattern match to "bucket" from test context
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
+  test "shows distinction between client and server", %{bucket: bucket} do
+    # Puts client to sleep
+    Process.sleep(100)
+    key = "milk"
+
+    val =
+      Agent.get_and_update(bucket, fn dict ->
+        # Puts server to sleep
+        Process.sleep(100)
+        Map.pop(dict, key)
+      end)
+
+    assert val == nil
+  end
 end
